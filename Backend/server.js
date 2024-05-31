32b40061f84403fb695edf0c15b0eff3aee9e9ba
oh-hyunjin <hyunjin6918@naver.com>
2024-05-31 13:01:57 +0900

    fix: 파일 분리 및 수정

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,14 @@
+// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const connection = require('./Database/db.js');
-
-
 app.use(bodyParser.json());
 
-// 모든 API 정보를 가져와서 보여주는 엔드포인트
-app.get('/apis', (req, res) => {
-  // 데이터 조회 쿼리 실행
-  const query = 'SELECT APIs.*, Users.user_name FROM APIs JOIN Users ON APIs.user_id = Users.user_id';
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).json({ error: 'Error executing query' });
-      return;
-    }
+const questionRoutes = require("./Routes/question");
+app.use("/api", questionRoutes);
 
-    // 결과를 JSON 형태로 응답
-    res.json(results);
-  });
+// 서버 시작
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
-})
-
-
