23d8a0d33653e1e9ecc04cef1a018c5bd040aeae
Hyesoo <kimhyesu914@gmail.com>
2024-06-06 19:18:32 +0900

    add : 메인페이지 테이블

--- Frontend/src/component/main/Table.js
+++ Frontend/src/component/main/Table.js
@@ -4,18 +4,40 @@ import { StyledTable, Th, Td } from './Style';
 
 const Table = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // axios를 사용하여 API 호출
     axios.get('http://localhost:8080/api/list/top?type=likes')
       .then(response => {
-        setData(response.data); 
-        
+        if (Array.isArray(response.data.result)) {
+          setData(response.data.result);
+          console.log('Fetched data:', response.data.result); // 데이터를 콘솔에 출력
+        } else {
+          console.error('Unexpected response data format:', response.data);
+          setError('Unexpected response data format.');
+        }
+        setLoading(false);
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
+        console.error('데이터를 가져오는 중 오류 발생:', error);
+        setError('서버에서 데이터를 불러오는 데 실패했습니다. 나중에 다시 시도해 주세요.');
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>{error}</p>
+        <pre>{error.toString()}</pre>
+      </div>
+    );
+  }
 
   return (
     <StyledTable>
@@ -28,14 +50,18 @@ const Table = () => {
         </tr>
       </thead>
       <tbody>
-        {data.map(item => (
-          <tr key={item.id}>
-            <Td><img src={item.favicon} alt="favicon" /></Td>
+        {data.length > 0 ? data.map(item => (
+          <tr key={item.api_id}>
+            <Td><img src={item.favicon} alt={`${item.name} favicon`} /></Td>
             <Td>{item.name}</Td>
             <Td>{item.category}</Td>
-            <Td>{item.price}</Td>
+            <Td>{item.pricepolicy}</Td>
           </tr>
-        ))}
+        )) : (
+          <tr>
+            <Td colSpan="4">데이터가 없습니다</Td>
+          </tr>
+        )}
       </tbody>
     </StyledTable>
   );
