90123fb9d05a871a6ab81dbc27ba54be24e75579
oh-hyunjin <hyunjin6918@naver.com>
2024-06-17 06:32:06 +0900

    feat: 좋아요 요청 / 요청 취소 / 상태 확인 기능 구현

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,28 @@
 // server.js
-var cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const connection = require('./Database/db');
+var cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./Database/db");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// API
 const apiRoutes = require("./Routes/apilist");
 app.use("/api/list", apiRoutes);
 const apiDataRoutes = require("./Routes/apiData");
 app.use("/api/data", apiDataRoutes);
+const likeRoutes = require("./Routes/likeApi");
+app.use("/api/like", likeRoutes);
 
+// 질문게시글
 const questionRoutes = require("./Routes/question");
 app.use("/api/question", questionRoutes);
 const questionCommentRoutes = require("./Routes/questionComment");
 app.use("/api/question/comment", questionCommentRoutes);
 
+// 자유게시글
 const generalRoutes = require("./Routes/general");
 app.use("/api/general/", generalRoutes);
 const generalCommentRoutes = require("./Routes/generalComment");
