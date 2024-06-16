6107b12339afd74315dd6ed8d8abb7c1db783fec
Hyesoo <kimhyesu914@gmail.com>
2024-06-16 22:02:20 +0900

    add:apidetail페이지

--- Frontend/src/component/searchResult/ApiCard.js
+++ Frontend/src/component/searchResult/ApiCard.js
@@ -8,7 +8,7 @@ const ApiCard = ({ icon, views, title, description }) => {
       <S.CardIcon>
         <img src={icon} alt="Icon" />
       </S.CardIcon>
-      <S.CardBookmark>
+      {/* <S.CardBookmark>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -23,7 +23,7 @@ const ApiCard = ({ icon, views, title, description }) => {
         >
           <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
         </svg>
-      </S.CardBookmark>
+      </S.CardBookmark> */}
       <S.CardViews>
         <svg
           xmlns="http://www.w3.org/2000/svg"
