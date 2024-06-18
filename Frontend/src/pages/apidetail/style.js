0a7b0bdefef7c3eacac34603122a8c668fac0815
Hyesoo <kimhyesu914@gmail.com>
2024-06-19 02:12:23 +0900

    add: 디테일페이지

--- Frontend/src/pages/apidetail/style.js
+++ Frontend/src/pages/apidetail/style.js
@@ -25,7 +25,7 @@ export const Favicon = styled.img`
   height: 100px;
 `;
 
-export const Example = styled.button`
+export const Example = styled.div`
   background-color: ${(props) => (props.isProvided ? "#00d44c" : "#888888")};
   color: white;
   font-size: 16px;
@@ -36,8 +36,9 @@ export const Example = styled.button`
   display: inline-block;
   text-align: center;
   cursor: pointer;
-  width: 150px;
+  width: max-content;
   text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2);
+  
 `;
 
 export const HeartButton = styled.button`
@@ -54,8 +55,8 @@ export const HeartButton = styled.button`
 `;
 
 export const GoButton = styled.button`
-  width: 200%;
-  height: 30%;
+  width: 100%;
+  height: 20%;
   background-color: #5060ff;
   border: none;
   border-radius: 30px;
@@ -78,6 +79,171 @@ export const GoButton = styled.button`
 `;
 
 export const ColDiv = styled.div`
+
   display: flex;
   flex-direction: column;
+  &:nth-child(1){
+    width:20%;
+  }
+  &:nth-child(2){
+    width:60%;
+  }
+  &:nth-child(3){
+    width:20%;
+    margin-left: 20px;
+    align-items:flex-end;
+    padding-bottom:20px;
+    
+    > :nth-child(2) {
+      margin-top: 30px;
+    }
+
+  > :last-child{
+     margin-top:75px;
+    }
+  }
+  
 `;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  margin-top: 30px;
+
+`;
+
+export const P = styled.p`
+border-bottom: 1px solid;
+padding-bottom: 20px;
+`
+export const Endpoint = styled.div`
+  border-radius: 8px;
+  width: fit-content;
+  margin-left: 100px;
+  color: #5060ff;
+  font-size: 25px;
+  font-weight: 700;
+  padding: 10px 20px;
+  padding-bottom:30px;
+  
+`;
+
+export const EndpointBox = styled.div`
+  width: max-content;
+  height: fit-content;
+  border:1px solid white;
+  border-radius: 20px;
+  background-color: white;
+  padding: 20px 10px;
+  display: flex;
+  p{
+    margin: 5px 0px 0px 15px;
+    font-size: 15px;
+    color:black;  white-space: nowrap; 
+    text-overflow: ellipsis;
+  }
+`;
+
+export const Method = styled.div`
+  width: 20%;
+  height: fit-content;
+  border-radius: 8px;
+  padding: 5px 10px;
+  background-color: #61AFFE;
+  color:white;
+  font-size: 16px;
+  
+`;
+export const Description= styled.p`
+font-size:10px;
+`
+
+// 테이블
+export const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Table = styled.table`
+  width: 45%;
+  background-color: black;
+  border-collapse: collapse;
+  margin: 25px 0;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+  table-layout: auto;
+`;
+
+export const Thead = styled.thead`
+  background-color: #f9fafb;
+  border-bottom: 2px solid #e8e8e8;
+  border-radius: 20px;
+  white-space: nowrap;
+`;
+
+export const Th = styled.th`
+  padding: 12px 15px;
+  text-align: left;
+  font-weight: 500;
+  color: #1f2937;
+  text-transform: uppercase;
+  font-size: 16px;
+`;
+
+export const Tbody = styled.tbody`
+  white-space: nowrap;
+`;
+
+export const Tr = styled.tr`
+  &:nth-child(even) {
+    background-color: black;
+  }
+`;
+
+export const Td = styled.td`
+  padding: 15px;
+  color: white;
+  font-size: 15px;
+`;
+
+export const StatusTag = styled.span`
+  display: inline-block;
+  padding: 0.25em 0.6em;
+  font-size: 0.875em; // 14px
+  font-weight: 600;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 10px;
+  color: ${props => props.textColor};
+  background-color: ${props => props.bgColor};
+`;
+
+export const RequiredTag = styled(StatusTag)`
+  color: ${props => (props.required ? '#742A2A' : 'rgb(34,84,61)')};
+  background-color: ${props => (props.required ? 'rgba(254,235,235,1)' : 'rgb(198, 246, 213)')};
+`;
+
+const statusColors = {
+  required: {
+    true: { textColor: '#fff', bgColor: '#e53e3e' }, // red for required
+    false: { textColor: '#fff', bgColor: '#48bb78' } // green for optional
+  },
+  type: {
+    string: { textColor: '#fff', bgColor: '#ed8936' }, // orange for string
+    integer: { textColor: '#fff', bgColor: '#4299e1' }, // blue for integer
+    default: { textColor: '#fff', bgColor: '#718096' } // gray for others
+  }
+};
+
+export const getStatusColor = (type, value) => {
+  if (!value) {
+    return { textColor: '#ffffff', bgColor: '#718096' }; 
+  }
+
+  if (type === 'required') {
+    return statusColors.required[value];
+  } else if (type === 'type') {
+    return statusColors.type[value.toLowerCase()] || statusColors.type.default;
+  }
+};
--- Frontend/src/pages/searchResult/DetailResult.js
+++ Frontend/src/pages/searchResult/DetailResult.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./Style";
 
 const DetailedView = ({ api, onClose }) => {
+  const navigate = useNavigate();
+
   if (!api) return null;
 
-  return (
-    <S.DetailedViewContainer>
-      <S.CloseButton onClick={onClose}>X</S.CloseButton>
-      <S.Pavicon src={api.favicon || "/img/default_api.png"} alt={api.title} />
-      <S.ApiName>{api.name}</S.ApiName>
-      <hr />
-      <S.SubTitle>API의 가격정책</S.SubTitle>
-      <p>{api.pricepolicy}</p>
-      <hr />
-      <S.SubTitle>About the API</S.SubTitle>
+  const handleDetailsClick = () => {
+    navigate(`/api-details/${api.api_id}`);
+  };
 
-      <p style={{ lineHeight: "1.5" }}>{api.description}</p>
-      <S.LinkButton>자세히 보기</S.LinkButton>
-    </S.DetailedViewContainer>
+  return (
+    <>
+      <S.DetailedViewContainer>
+        <S.CloseButton onClick={onClose}>X</S.CloseButton>
+        <S.Pavicon
+          src={api.favicon || "/img/default_api.png"}
+          alt={api.title}
+        />
+        <S.ApiName>{api.name}</S.ApiName>
+        <hr />
+        <S.SubTitle>API의 가격정책</S.SubTitle>
+        <p>{api.pricepolicy}</p>
+        <hr />
+        <S.SubTitle>About the API</S.SubTitle>
+        <p style={{ lineHeight: "1.5" }}>{api.description}</p>
+        <S.LinkButton onClick={handleDetailsClick}>자세히 보기</S.LinkButton>
+      </S.DetailedViewContainer>
+    </>
   );
 };
 
