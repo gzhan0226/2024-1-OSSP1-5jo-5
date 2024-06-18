6d2cf0ab720079880417989df4d3475447b2207a
Hyesoo <kimhyesu914@gmail.com>
2024-06-19 05:18:50 +0900

    메인페이지

--- Frontend/src/component/common/SearchBar.js
+++ Frontend/src/component/common/SearchBar.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import {
   SearchBarContainer,
   Bar,
   SearchInput,
   SearchButton,
   Icon,
-  FilterIconContainer,
-  FilterIcon,
 } from "./Style";
 import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ isDetailActive }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/api/list/search`,
+        { params: { search: searchTerm } }
+      );
+      navigate("/search-results", { state: { apiData: response.data } });
+    } catch (error) {
+      console.error("검색 중 오류 발생:", error);
+    }
+  };
+
   return (
     <SearchBarContainer style={{ width: isDetailActive ? "60vw" : "84vw" }}>
       <Bar>
         <SearchInput
           type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="검색하고자하는 API를 입력해보세요!"
         />
-        <SearchButton>
+        <SearchButton onClick={handleSearch}>
           <Icon as={FaSearch} />
         </SearchButton>
       </Bar>
--- Frontend/src/pages/main/style.js
+++ Frontend/src/pages/main/style.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 export const MainWrapper = styled.div`
   background-color: rgb(245, 245, 251);
-  width: 100vh;
+  width: 84vw;
 `;
 
 export const BannerWrapper = styled.div`
@@ -24,4 +24,14 @@ export const TableWrapper = styled.div`
   width: 86%;
   justify-content: space-between;
   margin-top: 4%;
+`;
+
+export const TableWrapper2 = styled.div`
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  width: 86%;
+  justify-content: space-between;
+  margin-top: 4%;
+  padding-bottom: 100px;
 `;
